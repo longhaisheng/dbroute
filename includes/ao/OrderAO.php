66d41ebaf40ef013357fb7a52fefb68ef40e3703
longhaisheng <longhaisheng20@163.com>
2014-08-14 22:29:59 +0800

    修改测试事务代码

--- includes/ao/OrderAO.php
+++ includes/ao/OrderAO.php
@@ -23,9 +23,8 @@ class OrderAO {
             $p1 = $this->orderModel->insert($user_id);//a
             $p2 = $this->orderGoodsModel->insert($user_id);//b
             $is_update = $this->cityModel->insert(); //未分表的表//c
-            $refund_id = $this->refundModel->insert($user_id);
-            
-          	print_r(cls_sqlexecute::get_database_name_list_in_one_transaction());
+//            $refund_id = $this->refundModel->insert($user_id);
+//            print_r(cls_sqlexecute::get_database_name_list_in_one_transaction());//加上 refundModel的insert将抛出异常,因为这样事务中将出现多个数据源
 
             $this->orderModel->getDbroute()->commit($tx_params);
             echo "order_id id is " . $p1 . " " . "order_goods_id is " . $p2 . " is_update:" . $is_update;
